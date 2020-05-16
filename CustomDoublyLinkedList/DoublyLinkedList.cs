70e945ee73474385c969e04c88467316b0b7af2a
valentin-zhelyazkov <56276041+valentin-zhelyazkov@users.noreply.github.com>
2020-05-16 17:00:30 +0300

    Add files via upload

---
+++ CustomDoublyLinkedList/DoublyLinkedList.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace CustomDoublyLinkedList
+{
+    public class DoublyLinkedList
+    {
+        private ListNode head;
+        private ListNode tail;
+
+        public int Count { get; private set; }
+
+        public void AddFirst(int element)
+        {
+            if (this.Count == 0)
+            {
+                this.head = this.tail = new ListNode(element);
+                
+            }
+            else
+            {
+                ListNode newHead = new ListNode(element);
+                ListNode oldHead = this.head;
+
+                this.head = newHead;
+                oldHead.PreviousNode = newHead;
+                newHead.NextNode = oldHead;
+            }
+            this.Count++;
+        }
+        public void AddLast(int element)
+        {
+            if(this.Count == 0)
+            {
+                this.head = this.tail = new ListNode(element);
+            }
+            else
+            {
+                ListNode newTail = new ListNode(element);
+                ListNode oldTail = this.tail;
+
+                this.tail = newTail;
+                oldTail.NextNode = newTail;
+                newTail.PreviousNode = oldTail;
+            }
+            this.Count++;
+        }
+        public int RemoveFirst()
+        {
+            if(this.Count == 0)
+            {
+                throw new InvalidOperationException("List is empty!");
+            }
+            else if(this.Count == 1)
+            {
+                this.head = null;
+                this.tail = null;
+
+            }
+            else
+            {
+                ListNode newHead = this.head;
+            }
+            this.Count--;
+            return 0;
+        }
+    }
+}
---
+++ CustomDoublyLinkedList/StartUp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CustomDoublyLinkedList
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            DoublyLinkedList doublyLinkedList = new DoublyLinkedList();
+
+            for (int i = 1; i <= 10; i++)
+            {
+                doublyLinkedList.AddFirst(i);
+            }
+
+            int[] arr = doublyLinkedList.ToArray();
+            
+        }
+    }
+}
